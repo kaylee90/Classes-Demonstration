60195e33376f1283305bfe5b6cb3c7df5d8f2fd7
Kaylee <ktyler2124@gmail.com>
2017-10-31 09:17:07 -0500

    Initial Upload
    Uploaded Classes Demonstration.

---
+++ ClassesDemonstration/ClassesDemonstration/Models/Pizza.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ClassesDemonstration.Models
+{
+    public class Pizza
+    {
+        public string Customer { get; set; }
+        //Size
+        public int Diameter { get; set; }
+
+        //# of slices
+        public int SliceCount { get; set; }
+
+        //Sauce Type
+        public string SauceType { get; set; }
+
+        //Cheese Type
+        public string CheeseType { get; set; }
+
+        //Toppings
+        public string[] Toppings { get; set; }
+
+        //CookTime
+        public TimeSpan CookTime { get; set; }
+
+        //Gluten free
+        public bool IsGlutenFree { get; set; }
+
+        //crust type
+        public CrustType TypeOfCrust { get; set; }
+
+        //Slice Type
+        public SliceType HowToSlice { get; set; }
+    }
+
+    public enum CrustType
+    {
+        Default = 0,
+        ThinCrust = 1,
+        Pan = 2,
+        HandTossed = 3,
+        DeepDish = 4,
+        Flat = 5
+    }
+
+    public enum SliceType
+    {
+        Default = 0,
+        Square = 1,
+        Regular = 2,
+        NoneAtAll = 3
+    }
+}
---
+++ ClassesDemonstration/ClassesDemonstration/Program.cs
@@ -0,0 +1,43 @@
+﻿using ClassesDemonstration.Models;
+using System;
+
+namespace ClassesDemonstration
+{
+    class Program
+    {
+        private static int gamestate;
+
+        private static int GameState { get; set; }
+
+        static void Main(string[] args)
+        {
+            Pizza pizza = new Pizza();
+            pizza.Customer = "Frank";
+            pizza.TypeOfCrust = CrustType.ThinCrust;
+            pizza.SauceType = "Marinara";
+            pizza.CheeseType = "Mozzerella";
+            pizza.Diameter = 21;
+
+            pizza.Toppings = new string[10];
+            pizza.Toppings[0] = "Green Peppers";
+            pizza.Toppings[1] = "Onion";
+            pizza.Toppings[2] = "Olives";
+            pizza.Toppings[3] = "Ham";
+            pizza.Toppings[4] = "Beef";
+            pizza.Toppings[5] = "Sasauge";
+            pizza.Toppings[6] = "Canadian Bacon";
+            pizza.Toppings[7] = "Bacon";
+            pizza.Toppings[8] = "Jalapenos";
+            pizza.Toppings[9] = "Pineapple";
+
+            pizza.CookTime = new System.TimeSpan(0, 8, 0);
+            pizza.HowToSlice = SliceType.Square;
+            pizza.SliceCount = 15;
+            pizza.IsGlutenFree = false;
+        }
+        private static void PrintPizza(Pizza pizza)
+        {
+            Console.WriteLine("Customer is: {0}", pizza.Customer);
+        }
+    }
+}
